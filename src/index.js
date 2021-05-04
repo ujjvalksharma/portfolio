8e6d1c7bd4b1001dd642ab1eb6abd99a65fe7844
ujjvalksharma <ujjvalksharma@gmail.com>
2021-05-04 13:16:31 -0400

    add particle js to reactjs

--- src/index.js
+++ src/index.js
@@ -1,15 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+//import App from './App';
 import reportWebVitals from './reportWebVitals';
-/*
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);*/
+import Particles from 'react-particles-js';
+import MouseParticles from 'react-mouse-particles'
+ReactDOM.render(<>
+  <MouseParticles g={1} color="random" cull="col,image-wrapper"/>
+<Particles
+  style={{ position: "fixed" }}
+  height="100%"
+  width="100%"
+  params={{
+    particles: {
+      color: {
+        value: "#ffffff"
+      },
+      line_linked: {
+        color: {
+          value: "#ffffff"
+        }
+      },
+      number: {
+        value: 100
+      },
+      size: {
+        value: 4
+      }
+    }
+  }}
+/>
+</>,
+  document.getElementById('particles-js')
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
